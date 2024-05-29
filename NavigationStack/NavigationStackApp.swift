6f1965c3ec66731c6138e8efd7adaddec4a7cc22
hooneun <kimhoon0316@gmail.com>
2024-05-29 18:42:26 +0900

    init

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Hoon on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
