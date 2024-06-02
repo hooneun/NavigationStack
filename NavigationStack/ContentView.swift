74dcd80acc50cee95e48817eee937de6663a3459
hooneun <kimhoon0316@gmail.com>
2024-06-02 15:23:44 +0900

    네비게이션 스택 & Destination & Link & Button

--- NavigationStack/ContentView.swift
+++ NavigationStack/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    let appleProducts = ["iPhone", "iPad", "Mac", "Apple Watch", "Apple TV"]
+//    @State private var stackPath: [String] = []
+    @State private var stackPath: NavigationPath = .init()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $stackPath) {
+            List(appleProducts, id: \.self) { product in
+                // NavigationLink Used
+                NavigationLink(value: product) {
+                    Text(product)
+                }
+
+                // Button Used
+//                Button {
+//                    stackPath.append(product)
+//                } label: {
+//                    Text(product)
+//                        .font(.largeTitle)
+//                        .bold()
+//                }
+            }
+            .navigationTitle("네비게이션 스택")
+            .navigationDestination(for: String.self) { product in
+                Text("\(product) has clicked")
+            }
+            .navigationDestination(for: Int.self) { _ in
+                Text("넘버가 선택되었습니다.")
+            }
+
+            Button {
+                stackPath.append(1)
+            } label: {
+                Text("Number 네비게이선")
+                    .font(.largeTitle)
+                    .bold()
+            }
         }
-        .padding()
     }
 }
 
